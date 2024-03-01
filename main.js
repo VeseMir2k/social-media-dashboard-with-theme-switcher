90cc2ef1902e8cc2c67709b077840295fefa0dff
Adam Stępień <vesemir2k@gmail.com>
2024-03-01 16:52:12 +0100

    Add prefers-color-scheme support

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
+// Selecting the dark mode toggle button element
 const toggle = document.querySelector(".dark-mode__toggle");
-const toggleSwitch = document.querySelector(".dark-mode__toggle-switch");
 
+// Checking if the user prefers a dark color scheme
+const isPrefersColorDark = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+).matches;
+
+// Initializing theme based on user's preference
+let theme = isPrefersColorDark;
+
+// Adding an event listener to the toggle button for toggling the theme
 toggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+  // Toggling between light and dark themes
+  if (theme) {
+    // Setting the data-theme attribute to dark
+    document.documentElement.setAttribute("data-theme", "dark");
+    // Updating theme variable
+    theme = !theme;
+  } else {
+    // Removing the data-theme attribute for light theme
+    document.documentElement.removeAttribute("data-theme", "dark");
+    // Updating theme variable
+    theme = !theme;
+  }
+  // Toggling the active class on the toggle button
   toggle.classList.toggle("active");
 });
+
+// Applying the initial theme based on user preference
+if (theme) {
+  // Setting the data-theme attribute to dark if the user prefers dark mode
+  document.documentElement.setAttribute("data-theme", "dark");
+  // Updating theme variable
+  theme = !theme;
+  // Toggling the active class on the toggle button
+  toggle.classList.toggle("active");
+} else {
+  // Updating theme variable if the user prefers light mode
+  theme = !theme;
+}
